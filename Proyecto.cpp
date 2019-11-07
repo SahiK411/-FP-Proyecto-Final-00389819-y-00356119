985dd45d327eb94c4d4fe31b240adfd012226c33
Diego Engelhard <diegoengelhard@Diegos-MacBook-Pro.local>
2019-11-07 11:10:38 -0600

    cambios menores

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -1,209 +1,247 @@
 #include <iostream>
-using namespace std;
-int maxHP = 20, currentHP, level = 1, gold = 0, experience = 0, equipmentWeapon = 0, equipmentArmor = 0;
+#include <math.h>
 
-void combate(int lowerRange, int upperRange, int hp, int rewardLower, int rewardUpper, int wepID, int armID){
+using namespace std;
+int maxHP = 20, currentHP, level = 1, gold = 0, experience = 0, equipmentWeapon = 0, equipmentArmor = 0, location;
+
+void combate(int lowerRange, int upperRange, int hp, int rewardLower, int rewardUpper){
     //Funcion que manejara el combate del juego.
     while(currentHP > 0 && hp > 0){
         
     }
 }
 
-void movimiento(int currLoc){
+int movimiento(){
+    int currLoc = location;
     switch(currLoc){
-        
+        case 1:
+
+        break;
+
+        case 2:
+
+        break;
+
+        case 3:
+
+        break;
+
+        case 4:
+
+        break;
+
+        case 5:
+
+        break;
+
+        case 6:
+
+        break;
+
+        case 7:
+
+        break;
+
+        case 8:
+
+        break;
+
+        case 9:
+
+        break;
     }
 }
 
 int menu(int options, string option1, string option2, string option3, string option4, string option5){
     int playerOption = 0; //Funcion para un menu reutilizable
-    switch options{
-        case 1:
-            cout << "\t1- " << option1 << endl;
-            cin >> playerOption;
-            break;
-        case 2:
-            cout << "\t1- " << option1 << endl;
-            cout << "\t2- " << option2 << endl;
-            cin >> playerOption;
-            break;
-        case 3:
-            cout << "\t1- " << option1 << endl;
-            cout << "\t2- " << option2 << endl;
-            cout << "\t3- " << option3 << endl;
-            cin >> playerOption;
-            break;
-        case 4:
-            cout << "\t1- " << option1 << endl;
-            cout << "\t2- " << option2 << endl;
-            cout << "\t3- " << option3 << endl;
-            cout << "\t4- " << option4 << endl;
-            cin >> playerOption;
-            break;
-        case 5:
-            cout << "\t1- " << option1 << endl;
-            cout << "\t2- " << option2 << endl;
-            cout << "\t3- " << option3 << endl;
-            cout << "\t4- " << option4 << endl;
-            cout << "\t5- " << option5 << endl;
-            cin >> playerOption;
-        default:
-            break;
-    }
+    switch (options){
+                case 1:
+                cout << "\t1- " << option1 << endl;
+                cin >> playerOption;
+                break;
+                case 2:
+                cout << "\t1- " << option1 << endl;
+                cout << "\t2- " << option2 << endl;
+                cin >> playerOption;
+                break;
+                case 3:
+                cout << "\t1- " << option1 << endl;
+                cout << "\t2- " << option2 << endl;
+                cout << "\t3- " << option3 << endl;
+                cin >> playerOption;
+                break;
+                case 4:
+                cout << "\t1- " << option1 << endl;
+                cout << "\t2- " << option2 << endl;
+                cout << "\t3- " << option3 << endl;
+                cout << "\t4- " << option4 << endl;
+                cin >> playerOption;
+                break;
+                case 5:
+                cout << "\t1- " << option1 << endl;
+                cout << "\t2- " << option2 << endl;
+                cout << "\t3- " << option3 << endl;
+                cout << "\t4- " << option4 << endl;
+                cout << "\t5- " << option5 << endl;
+                cin >> playerOption;
+                default:
+                break;
+        }
     return playerOption;
 }
 
 void tienda(){
     int x = 1, y; //Funcion para cambiar el arma y armadura del jugador mediante compras.
     string z;
-    cout << "You enter the store, various wares divided roughly into two different stalls.\n";
-    cout << "You can exit using '0'.\n"
-    while x != 0{
-        x = menu(2, "Purchase weapons.", "Purchase armor.");
+    cout << "Usted entra a la tienda. La mercancia se encuentra dividida en dos puestos distintos.\n";
+    cout << "Pulse '0' para salir.\n";
+    while (x != 0){
+        x = menu(2, "- Comprar armas.", "- Comprar armadura.", "","","");
         switch (x){
             case 1:
-                cout << "You walk towards their weapon selection, browsing for anything that catches your eye.\n";
-                cout << "You can exit using '0'.\n"
-                y = menu(5, "Training Sword - 20 gold" , "Rusty Halberd - 35 gold", "Silver Rapier - 40 gold", "Steel Broadsword - 50 gold", "Diamond-tipped Spear - 70 gold");
+                cout << "Bienvenido a la seccion de armas. Puede adquirir cualquier producto que le llame la atencion.\n";
+                cout << "Pulse '0' para salir.\n";
+                y = menu(5, "Espada de entrenamiento - 20 ORO" , "Alabarda Oxidada - 35 ORO", "Espada de plata - 40 ORO", " Sable de acero- 50 ORO", "Lanza de Oro - 70 ORO");
                 switch (y){
                     case 1:
-                        cout << "A small, brand-new training sword. +3 to your average damage, +0 to your defence.\n";
-                        cout << "Do you want to purchase it? Y/N\n";
-                            cin >> z;
-                        if (z == 'y'||"yes"){
+                        cout << "Pequena espada de entrenamiento. +3 de ataque, +0 de defensa.\n";
+                        cout << "Adquirir? S/N\n";
+                        cin >> z;
+                        if (z == "s" || "si" || "Si" || "S" || "SI"){
                             gold -= 20;
                             equipmentWeapon = 1;
                             break;
                         }
                         else
                         {
-                         break;   
+                            break;
                         }
                     case 2:
-                        cout << "A halberd that's seen better days. +6 to your average damage, +1 to your defence.\n";
-                        cout << "Do you want to purchase it? Y/N\n";
+                        cout << "Alabarda Oxidada. +6 de ataque, +1 de defensa.\n";
+                        cout << "Adquirir? S/N\n";
                         cin >> z;
-                        if (z == 'y'||"yes"){
+                        if (z == "s" || "si" || "Si" || "S" || "SI"){
                             gold -= 35;
                             equipmentWeapon = 2;
                             break;
                         }
                         else
                         {
-                         break;
+                            break;
                         }
                     case 3:
-                        cout << "A well-made rapier. Such a light weapon should allow you to dodge damage much more easily. +4 to your average damage, +4 to your defence.\n";
-                        cout << "Do you want to purchase it? Y/N\n";
+                        cout << "Espada de plata. Arma ligera, permitiendo mayor agilidad al momento del combaate. +4 de ataque, +4 de defensa.\n";
+                        cout << "Adquirir? S/N\n";
                         cin >> z;
-                        if (z == 'y'||"yes"){
+                        if (z == "s" || "si" || "Si" || "S" || "SI"){
                             gold -= 40;
                             equipmentWeapon = 3;
                             break;
                         }
                         else
                         {
-                         break;   
+                            break;
                         }
                     case 4:
-                        cout << "A simple-looking broadsword. The durability of the material means it can cut through most enemies with ease, though its heavy design means you'll be getting cut just as much as you cut others.\n +9 to your average damage, -3 to your defence.\n";
-                        cout << "Do you want to purchase it? Y/N\n";
+                        cout << "Sable de acero. El acero aporta una mayor durabilidad al arama, permite atravesar a los enemigos con facilidad. Debido al material de construccion, el hierro sacrifica movilidad a la hora del combate.\n +9 de ataque, -3 defensa.\n";
+                        cout << "Adquirir? S/N\n";
                         cin >> z;
-                        if (z == 'y'||"yes"){
+                        if (z == "s" || "si" || "Si" || "S" || "SI"){
                             gold -= 50;
                             equipmentWeapon = 4;
                             break;
                         }
                         else
                         {
-                         break;   
+                            break;
                         }
                     case 5:
-                        cout << "An ornate spear. With this weapon, you reckon you can pierce through any armor with ease.\n +9 to your average damage, +0 to your defence.\n";
-                        cout << "Do you want to purchase it? Y/N\n";
+                        cout << "Lanza de oro. La lanza de oro, permite atravesar cualquier armadura con facilidad.\n" ".\n +9 de ataque, +0 defensa.\n";
+                        cout << "Adquirir? S/N\n";
                         cin >> z;
-                        if (z == 'y'||"yes"){
+                        if (z == "s" || "si" || "Si" || "S" || "SI"){
                             gold -= 70;
                             equipmentWeapon = 5;
                             break;
                         }
                         else
                         {
-                         break;   
+                            break;
                         }
                     default:
-                    break;
+                        break;
                 }
                 break;
+
             case 2:
-                cout << "You walk towards some armor stands, quickly examining the wares on offer.\n";
-                cout << "You can exit using '0'.\n";
+                cout << "Bienvenido a la seccion de armaduras. Puede adquirir cualquier producto que le llame la atencion.\n";
+                cout << "Pulse '0' para salir..\n";
                 y = menu(5, "Padded clothing - 5 gold", "Leather armor - 15 gold", "Chainmail - 35 gold", "Steel under-armor - 40 gold", "Steel armor - 65 gold");
                 switch(y){
                     case 1:
-                        cout << "Plain clothes with a slight cotton under piece. Won't protect you from much, but the ease of movement should let you hit just a little harder.\n +2 to your average damage, +0 to your defence.\n";
-                        cout << "Do you want to purchase it? Y/N\n";
+                        cout << "Set de ropa. No te protejera de mucho, pero gracias a su peso ligero, aportara mayor movilidad en combate.\n +2 de ataque, +0 de defensa.\n";
+                        cout << "Adquirir? S/N\n";
                         cin >> z;
-                        if (z == 'y'||"yes"){
+                        if (z == "s" || "si" || "Si" || "S" || "SI"){
                             gold -= 5;
                             equipmentArmor = 1;
-                        break;
+                            break;
                         }
                         else
                         {
-                         break;   
+                            break;
                         }
                     case 2:
-                        cout << "A simple set of leather armor. Slight protection, on a budget.\n +0 to your average damage, +2 to your defence.\n";
-                        cout << "Do you want to purchase it? Y/N\n";
+                        cout << "Armadura de cuero. Aporta proteccion minima.\n +0 de ataque, +2 de defensa.\n";
+                        cout << "Adquirir? S/N\n";
                         cin >> z;
-                        if (z == 'y'||"yes"){
+                        if (z == "s" || "si" || "Si" || "S" || "SI"){
                             gold -= 15;
                             equipmentArmor = 2;
                             break;
                         }
                         else
                         {
-                         break;   
+                            break;
                         }
                     case 3:
-                        cout << "A sturdy set of chain-mail armor.\n +0 to your average damage, +4 to your defence.\n";
-                        cout << "Do you want to purchase it? Y/N\n";
+                        cout << "Armadura de Malla de metal.\n +0 de ataque, +4 de defensa.\n";
+                        cout << "Adquirir? S/N\n";
                         cin >> z;
-                        if (z == 'y'||"yes"){
+                        if (z == "s" || "si" || "Si" || "S" || "SI"){
                             gold -= 35;
                             equipmentArmor = 3;
                             break;
                         }
                         else
                         {
-                         break;
+                            break;
                         }
                     case 4:
-                        cout << "A set of steel plates outfitted for protection. Offers part of the protection of steel armor, with less of a downside.\n -3 to your average damage, +8 to your defence.\n";
-                        cout << "Do you want to purchase it? Y/N\n";
+                        cout << "Torso de una armadura de acero. Aporta una porcion de proteccion de una armadura de acero.\n -3 de ataque, +8 de defensa.\n";
+                        cout << "Adquirir? S/N\n";
                         cin >> z;
-                        if (z == 'y'||"yes"){
+                        if (z == "s" || "si" || "Si" || "S" || "SI"){
                             gold -= 40;
                             equipmentArmor = 4;
                             break;
                         }
                         else
                         {
-                         break;   
+                            break;
                         }
                     case 5:
-                        cout << "A set of sturdy steel armor pieces. Protects you from most enemies, though you won't be moving very fast while you wear it, giving enemies more chances to dodge.\n -6 to your average damage, +18 to your defence.\n";
-                        cout << "Do you want to purchase it? Y/N\n";
+                        cout << "Armadura completa de acerp. Proteje de la mayor parte de los enemigos, en su contraparte, sacrifica movilidad.\n -6 de ataque, +18 de defensa.\n";
+                        cout << "Adquirir? S/N\n";
                         cin >> z;
-                        if (z == 'y'||"yes"){
+                        if (z == "s" || "si" || "Si" || "S" || "SI"){
                             gold -= 65;
                             equipmentArmor = 5;
                             break;
                         }
                         else
                         {
-                         break;   
+                            break;
                         }
                 }
             default:
@@ -213,7 +251,7 @@ void tienda(){
 }
 
 void nivel(){
-    int func = math.floor(float(experience)/35) + 1;
+    int func = int(floor(float(experience)/35) + 1);
     if(func == level++){
         level++;
     }
@@ -222,11 +260,11 @@ void nivel(){
 void enemigo(int id){
     switch(id){
         case 0:
-            cout << "A lesser goblin approaches.\n";
+            cout << "Un duende se asoma a lo lejos.\n";
             combate(1, 6, 10, 5, 9);
             break;
         case 1:
-            cout << "You spot a wildcat stalking you from the foliage.\n";
+            cout << "Detectas un Tigre observandote desde un arbusto a  lejos.\n";
             combate(4, 10, 16, 5, 15);
             break;
         case 2:
@@ -238,9 +276,9 @@ void enemigo(int id){
 
 int main(){
     currentHP = maxHP;
-    int location = 0;
+    location = 0;
     bool inGame = true;
     while(inGame == true){
-        movimiento(0)
+        location = movimiento();
     }
 }
